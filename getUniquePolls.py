ba03d84a0849ba775b4f581a5674aba81e5bf705
Aaron Shaffer <aaron.c.shaffer@gmail.com>
2018-04-26 10:17:17 -0700

    added scraper for house.csv and senate.csv to extract all unique poll names and output to csv

---
+++ getUniquePolls.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+import csv
+
+polls = []
+
+def getPolls(fileName):
+    with open(fileName, 'r') as csvfile:
+        has_header = csv.Sniffer().has_header(csvfile.read(1024))
+        csvfile.seek(0)
+        reader = csv.reader(csvfile, delimiter=',')
+        if has_header:
+            next(reader)
+        for row in reader:
+            polls.append(row[1])
+
+getPolls("senate.csv")
+getPolls("house.csv")
+
+outFile = open("pollnames.csv","w")
+outFile.write("Poll\n")
+for poll in set(polls):
+    outFile.write(poll + "\n")
+outFile.close
