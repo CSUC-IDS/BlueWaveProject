20138087560a474ce7273eaabe29a4f184077e47
Aaron Shaffer <aaron.c.shaffer@gmail.com>
2018-04-26 09:42:38 -0700

    updated scrapeRCP.py to only grap senate and house data and to make unique CSVs for each

--- scrapeRCP.py
+++ scrapeRCP.py
@@ -2,26 +2,33 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-rcpurl = "https://www.realclearpolitics.com/epolls/latest_polls/"
-thepage = urlopen(rcpurl)
-soup = BeautifulSoup(thepage,"lxml")
+rcpURL_SENATE = "https://www.realclearpolitics.com/epolls/latest_polls/senate/"
+rcpURL_HOUSE = "https://www.realclearpolitics.com/epolls/latest_polls/house/"
 
-out_file = open("rcp.csv", "w")
+def make_csv(pageurl, fileName):
+    webPage = urlopen(pageurl)
 
-tables = soup.findAll('table', { "class" : None})
-sortables = soup.findAll('table', {"class": "sortable"})
-out_file.write("Date, Race, Poll, Results, Votes, Spread\n")
-for i in range(0,len(tables)):
-    date = tables[i].find('td', { "class" : "date" })
-    races = sortables[i].findAll('td', { "class" : "lp-race" })
-    polls = sortables[i].findAll('td', { "class" : "lp-poll" })
-    results = sortables[i].findAll('td', { "class" : "lp-results" })
-    spread = sortables[i].findAll('td', { "class" : "lp-spread" })
-    for i in range(0,len(races)):
-        da = date.find('b').contents[0].replace(',','')
-        ra = races[i].find('a').contents[0]
-        po = polls[i].find('a').contents[0]
-        re = results[i].find('a').contents[0].split(',')
-        sp = spread[i].find('span').contents[0]
-        for r in re:
-            out_file.write(da + ',' + ra + ',' + po + ',' + r.lstrip().replace(' ',',') + ',' + sp + '\n')
+    soup = BeautifulSoup(webPage,"lxml")
+
+    out_file = open(fileName, "w")
+    tables = soup.findAll('table', { "class" : None})
+    sortables = soup.findAll('table', {"class": "sortable"})
+    out_file.write("Date, Race, Poll, Results, Votes, Spread\n")
+    for i in range(0,len(tables)):
+        date = tables[i].find('td', { "class" : "date" })
+        races = sortables[i].findAll('td', { "class" : "lp-race" })
+        polls = sortables[i].findAll('td', { "class" : "lp-poll" })
+        results = sortables[i].findAll('td', { "class" : "lp-results" })
+        spread = sortables[i].findAll('td', { "class" : "lp-spread" })
+        for i in range(0,len(races)):
+            da = date.find('b').contents[0].replace(',','')
+            ra = races[i].find('a').contents[0]
+            po = polls[i].find('a').contents[0]
+            re = results[i].find('a').contents[0].split(',')
+            sp = spread[i].find('span').contents[0]
+            for r in re:
+                out_file.write(da + ',' + ra + ',' + po + ',' + r.lstrip().replace(' ',',') + ',' + sp + '\n')
+    out_file.close()
+
+make_csv(rcpURL_SENATE, "senate.csv")
+make_csv(rcpURL_HOUSE, "house.csv")
